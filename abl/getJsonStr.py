e6b9ea04e6954aea7d0244af99dae8070c4f5ac7
nelso <nelsonnick@126.com>
2021-04-05 19:57:07 +0800

    y

--- abl/getJsonStr.py
+++ abl/getJsonStr.py
@@ -28,79 +28,78 @@ str7 = ''
 str8 = ''
 str9 = ''
 stra = ''
-
-db = pymysql.connect("localhost", "root", "root", "abl")
+db = pymysql.connect(host="localhost", user="root", password="root", database="abl")
 
 cursor = db.cursor()
-cursor.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior="+"01")
+cursor.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior='01'")
 data = cursor.fetchall()
 for d1 in data:
     cursor1 = db.cursor()
-    cursor1.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior=" + str(d1[0]))
+    cursor1.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior='" + str(d1[0])+"'")
     if cursor1.rowcount == 0:
-        str1 = str1 + '{"label":"' + d1[2] + '","value":"' + d1[0] + '},'
+        str1 = str1 + '{"label":"' + d1[2] + '","value":"' + d1[0] + '"},'
     else:
         data1 = cursor1.fetchall()
         for d2 in data1:
             cursor2 = db.cursor()
-            cursor2.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior=" + str(d2[0]))
+            cursor2.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior='" + str(d2[0]) + "'")
             if cursor2.rowcount == 0:
-                str2 = str2 + '{"label":"' + d2[2] + '","value":"' + d2[0] + '},'
+                str2 = str2 + '{"label":"' + d2[2] + '","value":"' + d2[0] + '"},'
             else:
                 data2 = cursor2.fetchall()
                 for d3 in data2:
                     cursor3 = db.cursor()
-                    cursor3.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior=" + str(d3[0]))
+                    cursor3.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior='" + str(d3[0]) + "'")
                     if cursor3.rowcount == 0:
-                        str3 = str3 + '{"label":"' + d3[2] + '","value":"' + d3[0] + '},'
+                        str3 = str3 + '{"label":"' + d3[2] + '","value":"' + d3[0] + '"},'
                     else:
                         data3 = cursor3.fetchall()
                         for d4 in data3:
                             cursor4 = db.cursor()
-                            cursor4.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior=" + str(d4[0]))
+                            cursor4.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior='" + str(d4[0]) + "'")
                             if cursor4.rowcount == 0:
-                                str4 = str4 + '{"label":"' + d4[2] + '","value":"' + d4[0] + '},'
+                                str4 = str4 + '{"label":"' + d4[2] + '","value":"' + d4[0] + '"},'
                             else:
                                 data4 = cursor4.fetchall()
                                 for d5 in data4:
                                     cursor5 = db.cursor()
-                                    cursor5.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior=" + str(d5[0]))
+                                    cursor5.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior='" + str(d5[0]) + "'")
                                     if cursor5.rowcount == 0:
-                                        str5 = str5 + '{"label":"' + d5[2] + '","value":"' + d5[0] + '},'
+                                        str5 = str5 + '{"label":"' + d5[2] + '","value":"' + d5[0] + '"},'
                                     else:
                                         data5 = cursor5.fetchall()
                                         for d6 in data5:
                                             cursor6 = db.cursor()
-                                            cursor6.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior=" + str(d6[0]))
+                                            cursor6.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior='" + str(d6[0]) + "'")
                                             if cursor6.rowcount == 0:
-                                                str6 = str6 + '{"label":"' + d6[2] + '","value":"' + d6[0] + '},'
+                                                str6 = str6 + '{"label":"' + d6[2] + '","value":"' + d6[0] + '"},'
                                             else:
                                                 data6 = cursor6.fetchall()
                                                 for d7 in data6:
                                                     cursor7 = db.cursor()
-                                                    cursor7.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior=" + str(d7[0]))
+                                                    cursor7.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior='" + str(d7[0]) + "'")
                                                     if cursor7.rowcount == 0:
-                                                        str7 = str7 + '{"label":"' + d7[2] + '","value":"' + d7[0] + '},'
+                                                        str7 = str7 + '{"label":"' + d7[2] + '","value":"' + d7[0] + '"},'
                                                     else:
                                                         data7 = cursor7.fetchall()
                                                         for d8 in data7:
                                                             cursor8 = db.cursor()
-                                                            cursor8.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior=" + str(d8[0]))
+                                                            cursor8.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior='" + str(d8[0]) + "'")
                                                             if cursor8.rowcount == 0:
-                                                                str8 = str8 + '{"label":"' + d8[2] + '","value":"' + d8[0] + '},'
+                                                                str8 = str8 + '{"label":"' + d8[2] + '","value":"' + d8[0] + '"},'
                                                             else:
                                                                 data8 = cursor8.fetchall()
                                                                 for d9 in data8:
                                                                     cursor9 = db.cursor()
-                                                                    cursor9.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior=" + str(d9[0]))
+                                                                    cursor9.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior='" + str(d9[0]) + "'")
                                                                     if cursor9.rowcount == 0:
-                                                                        str9 = str9 + '{"label":"' + d9[2] + '","value":"' + d9[0] + '},'
+                                                                        str9 = str9 + '{"label":"' + d9[2] + '","value":"' + d9[0] + '"},'
                                                                     else:
                                                                         data9 = cursor9.fetchall()
                                                                         for da in data9:
-                                                                            stra = stra + '{"label":"' + da[2] + '","value":"' + da[0] + '},'
-
-
+                                                                            cursora = db.cursor()
+                                                                            cursora.execute("SELECT _id, superior, showname, istype FROM abl WHERE superior='" + str(da[0]) + "'")
+                                                                            stra = stra + '{"label":"' + da[2] + '","value":"' + da[0] + '"},'
                                                                         str9 = str9 + '{"label":"' + d9[2] + '","value":"' + d9[0] + '","children":[' + stra[:-1] + ']},'
                                                                         stra = ''
                                                                 str8 = str8 + '{"label":"' + d8[2] + '","value":"' + d8[0] + '","children":[' + str9[:-1] + ']},'
@@ -119,7 +118,7 @@ for d1 in data:
                 str3 = ''
         str1 = str1 + '{"label":"' + d1[2] + '","value":"' + d1[0] + '","children":[' + str2[:-1] + ']},'
         str2 = ''
-str0 = 'export var data = [' + str1[:-1] + ']'
+str0 = '[' + str1[:-1] + ']'
 # 关闭数据库连接
 db.close()
 print(str0)
